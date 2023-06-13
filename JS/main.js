cd21a13eea0854895eaf429d2e01c31d16ee6116
ramedina98 <b_stone99@outlook.es>
2023-06-13 12:52:37 -0600

    Se avanzo con la extraccion de informacion del archivo JSON

--- JS/main.js
+++ JS/main.js
@@ -4,3 +4,18 @@ btnSwitch.addEventListener('click', () => {
     document.body.classList.toggle('dark');
     btnSwitch.classList.toggle('active');
 });
+
+//we open the JSON file... 
+const request =  new XMLHttpRequest();
+request.open("GET", "/articulos.JSON", true);
+request.onreadystatechange = function(){
+    if(request.readyState === 4 && request.status === 200) {
+        const contenidoJSON = request.responseText;
+        const objetoJSON = JSON.parse(contenidoJSON);
+    
+        // Procesar el objeto JSON
+        console.log("Imagen: " + objetoJSON[11].imagen);
+        console.log("Extracto: " + objetoJSON[11].extracto);
+    }
+};
+request.send();
