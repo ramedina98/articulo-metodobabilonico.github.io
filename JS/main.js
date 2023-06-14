1c5224395f0121b053ca35518a654d35b5c951a1
ramedina98 <b_stone99@outlook.es>
2023-06-14 08:47:33 -0600

    Se siguio trabajando con el JSON y en el JS mian se empezo la funcion creat noti

--- JS/main.js
+++ JS/main.js
@@ -1,21 +1,31 @@
 const btnSwitch = document.querySelector('.switch');
+const mainElement = document.querySelector('.cont_car_all');
 //Este codigo es para trabajar el modo dark
 btnSwitch.addEventListener('click', () => {
     document.body.classList.toggle('dark');
     btnSwitch.classList.toggle('active');
 });
-
+//this function helps us to show in little articles the info...
+function makeArticles(imagen, subT, extracto, link){
+    //start creating the elements... 
+    const father = document.createElement('div');
+    father.classList.add('cont_car');
+}
 //we open the JSON file... 
 const request =  new XMLHttpRequest();
 request.open("GET", "/articulos.JSON", true);
 request.onreadystatechange = function(){
     if(request.readyState === 4 && request.status === 200) {
-        const contenidoJSON = request.responseText;
-        const objetoJSON = JSON.parse(contenidoJSON);
-    
-        // Procesar el objeto JSON
-        console.log("Imagen: " + objetoJSON[11].imagen);
-        console.log("Extracto: " + objetoJSON[11].extracto);
+        const contentJSON = request.responseText;
+        const article = JSON.parse(contentJSON);
+        //here we show the info in JSON file...
+        makeArticles(article[0].imagen,article[0].subTitulo, 
+            article[0].extracto, article[0].linkWeb)
+
+        console.log("Imagen: " + article[0].imagen);
+        console.log("Sub titulo: " + article[0].subTitulo);
+        console.log("Extracto: " + article[0].extracto);
+        console.log("Link al articulo: " + article[0].linkWeb);
     }
 };
 request.send();
