30dfd38c1bb1af107c8758c2d6e59acfb88b566a
ramedina98 <b_stone99@outlook.es>
2023-06-14 10:55:26 -0600

    Termine el JSON file, termine de crear la funcion que genera los articulos y obtiene la infrmacion del JSON, y se modificaron algunas caracteristicas del style.css...

--- JS/main.js
+++ JS/main.js
@@ -6,10 +6,55 @@ btnSwitch.addEventListener('click', () => {
     btnSwitch.classList.toggle('active');
 });
 //this function helps us to show in little articles the info...
-function makeArticles(imagen, subT, extracto, link){
-    //start creating the elements... 
+function makeArticles(imagen, subT, sublink , axtracto, link){
+    //start creating the elements...
+    //This is the main div that content all the itemes of the article...
     const father = document.createElement('div');
     father.classList.add('cont_car');
+    //
+    const linkWeb = document.createElement('a');
+    linkWeb.classList.add('linkWeb');
+    linkWeb.href = `${link}`;
+    //this div content the img...
+    const imgDestacada = document.createElement('div');
+    imgDestacada.classList.add('cont_img');
+    imgDestacada.style.backgroundImage = `url(${imagen})`;
+    //this div content the sub title and the abstract...
+    const abstractNtitle = document.createElement('div');
+    abstractNtitle.classList.add('cont_info_car');
+    //child div of abstracNtitle, this has the sub title... 
+    const subDiv = document.createElement('div');
+    subDiv.classList.add('tema');
+    //
+    const tema = document.createElement('a');
+    tema.classList.add('etiquetass');
+    tema.href = `${sublink}`;
+    //this is the span that is in subDiv...
+    const subTitle = document.createElement('span');
+    subTitle.innerText = `${subT}`;
+    //child div of abstracNtitle, this has the abstrac...
+    const abstractDiv = document.createElement('div');
+    abstractDiv.classList.add('pre');
+    //this is the <p></p> (abstrac)...
+    const abstracText = document.createElement('p');
+    abstracText.innerText = `${axtracto}`;
+
+    mainElement.appendChild(father);
+    father.appendChild(linkWeb);
+    linkWeb.appendChild(imgDestacada);
+    father.appendChild(abstractNtitle);
+    abstractNtitle.appendChild(subDiv);
+    subDiv.appendChild(tema);
+    tema.appendChild(subTitle);
+    abstractNtitle.appendChild(abstractDiv);
+    abstractDiv.appendChild(abstracText);
+}
+//this function makes randonme numbers...
+function RandomeNumbers() {
+    const numAleatorio = Math.random();
+    const numEntero = Math.floor(numAleatorio * 24); // Genera números del 0 al 23
+
+    return numEntero;
 }
 //we open the JSON file... 
 const request =  new XMLHttpRequest();
@@ -19,13 +64,16 @@ request.onreadystatechange = function(){
         const contentJSON = request.responseText;
         const article = JSON.parse(contentJSON);
         //here we show the info in JSON file...
-        makeArticles(article[0].imagen,article[0].subTitulo, 
-            article[0].extracto, article[0].linkWeb)
-
-        console.log("Imagen: " + article[0].imagen);
-        console.log("Sub titulo: " + article[0].subTitulo);
-        console.log("Extracto: " + article[0].extracto);
-        console.log("Link al articulo: " + article[0].linkWeb);
+        let x = 0; 
+        do{
+            /*I have to writte a function that check if a 
+            randome number is repeated or not...*/
+            let j = 0;
+            j = RandomeNumbers();
+            makeArticles(article[j].imagen, article[j].subTitulo, 
+                article[j].subLink, article[j].extracto, article[j].linkWeb);
+                x++;
+        }while(x !== 3);
     }
 };
 request.send();
